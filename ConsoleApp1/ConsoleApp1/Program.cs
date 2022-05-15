9601fd82585aa11ecf7df3e46154372a4678b217
1MuRaD12 <tu71tfsws@code.edu.az>
2022-05-15 07:27:05 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Capacity");
+            int capaCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Count");
+            int Count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("IsAuto");
+            bool IsAuto = Convert.ToBoolean(Console.ReadLine());
+            Weapon weapon = new Weapon(capaCapacity, Count, IsAuto);
+            int check = 0;
+            do
+            {
+                Console.WriteLine("0   informasya almaq");
+                Console.WriteLine("1   shoot method");
+                Console.WriteLine("2   Fire method");
+                Console.WriteLine("3   GetRaminBulletCount");
+                Console.WriteLine("4   Relod method");
+                Console.WriteLine("5   ChangeFireMode");
+                Console.WriteLine("6   exit");
+                 check = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (check)
+                {
+                    case 0:
+                        weapon.Getinfo();
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        weapon.GetRemainBullentCount();
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 6:
+                        Console.WriteLine("END");
+                        break;
+                    default:
+                        Console.WriteLine("Entered incorrectly");
+                        break;
+
+                }
+
+            } while (check != 6);
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Weapon.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Weapon
+    {
+        public static int Capacity { get; set; }
+        public static int Count { get; set; }
+        public static bool IsAuto { get; set; }
+
+
+        public Weapon(int capacity, int count, bool isauto)
+        {
+            Capacity = capacity;
+            Count = count;
+            IsAuto = isauto;
+        }
+
+
+
+        public void Shoot()
+        {
+            if (IsAuto == false)
+            {
+                Count = Count - 1;
+            }
+            Console.WriteLine(Count);
+
+        }
+
+        public void Fire()
+        {
+            if (IsAuto == true)
+            {
+                for (int i = 0; i < Count; i--)
+                {
+                    if (Count > 0)
+                    {
+                        Count--;
+                        Console.WriteLine(Count);
+                    }
+                }
+            }
+        }
+
+
+        public int GetRemainBullentCount()
+        {
+            return Capacity - Count;
+        }
+
+
+        public void Reload()
+        {
+            Console.WriteLine(Capacity = Count + GetRemainBullentCount());
+        }
+
+
+        public void ChangeFireMode()
+        {
+            if (IsAuto == false)
+            {
+                Shoot();
+            }
+            else if (IsAuto == true)
+            {
+                Fire();
+            }
+         
+
+        }
+        public void Getinfo()
+        {
+            Console.WriteLine("1 Shoot ");
+            Console.WriteLine("2 Fire ");
+            Console.WriteLine("3 GetRemainBulletCount ");
+            Console.WriteLine("4 Reload ");
+            Console.WriteLine("5 ChangeFireMode");
+
+        }
+    }
+}
